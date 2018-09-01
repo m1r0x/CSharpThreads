5ba7783cd5a4d4f92e5fe9a2f4ed120ee577e6c1
Strati <m1r0x@live.de>
2018-09-01 10:01:47 +0200

    initial commit

--- CSharpThreads/Program.cs
+++ CSharpThreads/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace CSharpThreads
 {
@@ -10,6 +11,39 @@ namespace CSharpThreads
     {
         static void Main(string[] args)
         {
+            Thread t = new Thread(MyThread1);
+            Thread t2 = new Thread(MyThread2);
+
+            t.Start();
+            t2.Start();
+
+            Counter = 0;
+
+            Console.ReadKey();
+
         }
+       
+
+        static void MyThread1()
+        {
+            for (int i = 0; i < 500; i++)
+            {
+                Console.WriteLine("Thread 1A counts: " + Counter);
+                Counter++;
+                Console.WriteLine("Thread 1B counts: " + Counter);
+            }
+        }
+
+        static void MyThread2()
+        {
+            for (int i = 0; i < 500; i++)
+            {
+                Console.WriteLine("Thread 2A counts: " + Counter);
+                Counter++;
+                Console.WriteLine("Thread 2B counts: " + Counter);
+            }
+        }
+
+        public static int Counter { get; set; }
     }
 }
